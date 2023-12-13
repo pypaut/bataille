74dfc760398116b23b922b5e62e0f0de1b8e3cfb
Geoffrey JOUNT <gjount@scaleway.com>
2023-12-13 11:42:22 +0100

    Massive refactor

--- internal/game/game.go
+++ internal/game/game.go
@@ -30,6 +30,8 @@ type Game struct {
 	ShowCards   bool
 	DrawOptions ebiten.DrawImageOptions
 
+	CurrentWin int
+
 	PlayerOneMessage string
 	PlayerTwoMessage string
 	PlayerOneWins    string
@@ -87,68 +89,133 @@ func NewGame() *Game {
 
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
-func (g *Game) Update() error {
+func (g *Game) Update() (err error) {
 	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
 		return errors.New("Game done!")
 	}
 
 	justClicked := inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft)
 
+	// Hide cards and re-distribute according to winner
 	if g.ShowCards && justClicked {
 		g.ShowCards = false
-		g.PlayerOneWins = ""
-		g.PlayerTwoWins = ""
 
-		winValue, err := g.DeckOne.WinsAgainst(g.DeckTwo)
-		if err != nil {
+		if g.CurrentWin == 1 {
+			g.GiveCardsToPlayerOne()
+
+		} else if g.CurrentWin == -1 {
+			g.GiveCardsToPlayerTwo()
+
+		} else if g.CurrentWin == 0 {
+			if err = g.CheckCardsAreDuplicates(); err != nil {
+				return err
+			}
+
+			g.MoveFrontCardsToBack()
+		}
+
+		g.ResetWinBadges()
+
+	} else if justClicked {
+		g.ShowCards = true
+
+		if err = g.UpdateCurrentWin(); err != nil {
 			return err
 		}
 
-		if winValue == 1 {
-			// Add cards to back of DeckOne
-			g.DeckOne.Cards = append(
-				g.DeckOne.Cards,
-				g.DeckOne.Cards[0],
-				g.DeckTwo.Cards[0],
-			)
-
-			g.PlayerOneWins = "+"
-
-		} else if winValue == -1 {
-			// Add cards to back of DeckTwo
-			g.DeckTwo.Cards = append(
-				g.DeckTwo.Cards,
-				g.DeckTwo.Cards[0],
-				g.DeckOne.Cards[0],
-			)
-
-			g.PlayerTwoWins = "+"
-
-		} else if winValue == 0 { // Draw
-			// Check if duplicate
-			if g.DeckOne.Cards[0].Color == g.DeckTwo.Cards[0].Color {
-				c := g.DeckOne.Cards[0]
-				errorMsg := fmt.Sprintf("duplicate card: %d, %d", c.Color, c.Value)
-				return errors.New(errorMsg)
-			}
-
-			g.PlayerOneWins = ""
-			g.PlayerTwoWins = ""
-
-			// Move both front cards to the back
-			g.DeckOne.Cards = append(g.DeckOne.Cards, g.DeckOne.Cards[0])
-			g.DeckTwo.Cards = append(g.DeckTwo.Cards, g.DeckTwo.Cards[0])
-		} else if winValue == -2 {
-			fmt.Printf("Error: unknown value")
-		}
-
-		// Rm first cards
-		g.DeckOne.Cards = g.DeckOne.Cards[1:]
-		g.DeckTwo.Cards = g.DeckTwo.Cards[1:]
-	} else if justClicked {
-		g.ShowCards = true
+		g.UpdateWinBadges()
 	}
 
+	err = g.CheckTotalNumberOfCards()
+	if err != nil {
+		return err
+	}
+
+	g.UpdatePlayersMessages()
+
+	return nil
+}
+
+func (g *Game) UpdatePlayersMessages() {
+	g.PlayerOneMessage = fmt.Sprintf("%d", len(g.DeckOne.Cards))
+	g.PlayerTwoMessage = fmt.Sprintf("%d", len(g.DeckTwo.Cards))
+}
+
+func (g *Game) UpdateCurrentWin() error {
+	winValue, err := g.DeckOne.WinsAgainst(g.DeckTwo)
+	if err != nil {
+		return err
+	}
+
+	g.CurrentWin = winValue
+	return nil
+}
+
+func (g *Game) UpdateWinBadges() {
+	if g.CurrentWin == 1 {
+		g.PlayerOneWins = "+"
+		g.PlayerTwoWins = ""
+	} else if g.CurrentWin == -1 {
+		g.PlayerTwoWins = "+"
+		g.PlayerOneWins = ""
+	} else {
+		g.PlayerTwoWins = ""
+		g.PlayerOneWins = ""
+	}
+}
+
+func (g *Game) ResetWinBadges() {
+	g.PlayerTwoWins = ""
+	g.PlayerOneWins = ""
+}
+
+func (g *Game) GiveCardsToPlayerOne() {
+	// Add cards to back of DeckOne
+	g.DeckOne.Cards = append(
+		g.DeckOne.Cards,
+		g.DeckOne.Cards[0],
+		g.DeckTwo.Cards[0],
+	)
+
+	// Remove cards from both
+	g.DeckOne.Cards = g.DeckOne.Cards[1:]
+	g.DeckTwo.Cards = g.DeckTwo.Cards[1:]
+}
+
+func (g *Game) GiveCardsToPlayerTwo() {
+	// Add cards to back of DeckTwo
+	g.DeckTwo.Cards = append(
+		g.DeckTwo.Cards,
+		g.DeckTwo.Cards[0],
+		g.DeckOne.Cards[0],
+	)
+
+	// Remove cards from both
+	g.DeckOne.Cards = g.DeckOne.Cards[1:]
+	g.DeckTwo.Cards = g.DeckTwo.Cards[1:]
+}
+
+func (g *Game) MoveFrontCardsToBack() {
+	// Duplicate front cards to back
+	g.DeckOne.Cards = append(g.DeckOne.Cards, g.DeckOne.Cards[0])
+	g.DeckTwo.Cards = append(g.DeckTwo.Cards, g.DeckTwo.Cards[0])
+
+	// Remove front cards
+	g.DeckOne.Cards = g.DeckOne.Cards[1:]
+	g.DeckTwo.Cards = g.DeckTwo.Cards[1:]
+}
+
+func (g *Game) CheckCardsAreDuplicates() error {
+	if g.DeckOne.Cards[0].Color == g.DeckTwo.Cards[0].Color {
+		c := g.DeckOne.Cards[0]
+		errorMsg := fmt.Sprintf("duplicate card: %d, %d", c.Color, c.Value)
+		return errors.New(errorMsg)
+	}
+
+	return nil
+}
+
+func (g *Game) CheckTotalNumberOfCards() error {
 	if len(g.DeckOne.Cards)+len(g.DeckTwo.Cards) != 52 {
 		errorMsg := fmt.Sprintf(
 			"game should contain 52 cards (D1: %d, D2: %d)",
@@ -158,9 +225,6 @@ func (g *Game) Update() error {
 
 		return errors.New(errorMsg)
 	}
-
-	g.PlayerOneMessage = fmt.Sprintf("%d", len(g.DeckOne.Cards))
-	g.PlayerTwoMessage = fmt.Sprintf("%d", len(g.DeckTwo.Cards))
 
 	return nil
 }
